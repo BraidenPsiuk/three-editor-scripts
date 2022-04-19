e56398a76d924660cb0210a115bb66c099a48553
Braiden Psiuk <33185352+BraidenPsiuk@users.noreply.github.com>
2022-04-19 18:09:44 -0400

    Update gltf-material-opacity-animate.js

--- gltf-material-opacity-animate.js
+++ gltf-material-opacity-animate.js
@@ -1,13 +1,10 @@
 const clock = new THREE.Clock();
 const opacityFadeInOut = this.parent.animations[0];
+this.material.transparent = true;
 
 const mixer = new THREE.AnimationMixer(this.parent);
 const action = mixer.clipAction( opacityFadeInOut );
 action.play();
-
-console.log(this.transparent);
-console.log(this.opacity);
-console.log(this);
 
 function update( event ) {
 	mixer.update( clock.getDelta() );
