6ec9c2eb04b498ced03e37678c76a0d40cdbb75b
Braiden Psiuk <33185352+BraidenPsiuk@users.noreply.github.com>
2022-04-21 21:41:06 -0400

    Create gltf-dolly-zoom-animate.js

---
+++ gltf-dolly-zoom-animate.js
@@ -0,0 +1,14 @@
+const clock = new THREE.Clock();
+player.setCamera(this);
+
+const mixer = new THREE.AnimationMixer(this.parent.parent);
+const action = mixer.clipAction( this.parent.parent.animations[0] );
+action.play();
+
+function update( event ) {
+	mixer.update( clock.getDelta() );
+  // Even though we directly animated the camera's position in Blender, the glTF exporter OR importer (not sure which) actually seems to
+  // create parent containers for all cameras in the scene, and these parent containers have their positions animated instead of the camera directly.
+	console.log(this.parent.position.z);
+	//this.material.opacity = this.children[0].scale.x;
+}
